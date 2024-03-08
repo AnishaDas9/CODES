34d6002a8e640b08d1ffe84201b757f85e1f201c
AnishaDas9 <dasanisha658@gmail.com>
2024-03-08 16:11:18 +0530

    basicC++

---
+++ datatypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 123;
+    cout << a << endl;
+    char b = 'v';
+    cout << b << endl;
+    float c = 1.2;
+    cout<< c << endl;
+    double d = 12.3;
+    cout << d << endl;
+    bool bl = true;
+    cout<< bl << endl;
+    int size = sizeof(a);
+    cout<< "size of a is:"<<size<< endl;
+    
+}
---
+++ operators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 2;
+    int b = 3;
+    bool first = (a==b);
+    cout<< first<< endl;
+
+}
