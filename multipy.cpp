e48da4574db5c19e958728cc78685da89ec67436
AnishaDas9 <dasanisha658@gmail.com>
2024-04-13 16:18:30 +0530

    multiply

---
+++ multipy.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter first no." <<endl;
+    cin>>a;
+    int b;
+    cout<<"Enter second no." <<endl;
+    cin>>b;
+
+    int c=a*b;
+    cout<<"Your result is:- " <<c <<endl;
+}
